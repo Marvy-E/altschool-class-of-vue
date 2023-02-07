4cc8fd83f78302f2d32d6f5b8515a6f3107b80fc
Marvelous Ejiogu <ejiogumarvelous@gmail.com>
2023-02-07 21:02:24 +0100

    Updates

---
+++ src/composables/color-picker.js
@@ -0,0 +1,16 @@
+import { ref } from "vue";
+
+export const useColorPickerGame = (colorsArr) => {
+  const colors = colorsArr;
+  let message = ref("Pick a color...");
+
+  const matchColor = (value) => {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    colors[randomNumber] === value
+      ? (message.value = `You win... [answer: $colorsArr[randomNumber]]`)
+      : (message.value = `You lose... [answer: $colorsArr[randomNumber]]`);
+  };
+
+  return { colors, message, matchColor };
+};
